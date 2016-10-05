c277c8b23496ca09560bee4d744c07fe59a7fbf5
JunSeok Seon <junseokseon@naver.com>
2016-10-05 16:34:06 +0900

    #git 프로젝트 반영

--- src/com/js/HelloWorld.java
+++ src/com/js/HelloWorld.java
@@ -1,12 +1,15 @@
+package com.js;
 
 public class HelloWorld {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		
 		System.out.println("Hello World");
 		System.out.println("My World");
 		System.out.println("too Much");
 		System.out.println("So Hot");
+
 	}
 
 }

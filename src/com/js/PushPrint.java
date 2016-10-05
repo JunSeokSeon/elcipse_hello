d7a1e312e884188c68ab5829be7cb573bdccd14d
JunSeok Seon <junseokseon@naver.com>
2016-10-05 17:14:28 +0900

    #Local Master Change

--- src/com/js/PushPrint.java
+++ src/com/js/PushPrint.java
@@ -6,6 +6,7 @@ public class PushPrint {
 		// TODO Auto-generated method stub
 		System.out.println("Add Java Class");
 		System.out.println("Master Change Class");
+		System.out.println("Local Master Hi");
 	}
 
 }
